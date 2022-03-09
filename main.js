423fb915881a2a6b585ccd8c3006c73b26ed1a63
Hen <hentzalah@gmail.com>
2022-03-09 20:49:45 +0200

    last commit

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
+
 container = document.querySelector(".container");
 let spinBtn = document.getElementById("spin");
 let resBtn = document.getElementById("reset");
+let link = document.getElementById("link");
+
 
 var deg = Math.ceil(Math.random() * 1000);
 
 
-
-let link = document.getElementById("link");
-
-let one = document.querySelector(".one");
-
-
-
-
-spinBtn.onclick = function () { //add reset botton
+spinBtn.onclick = function () {
     
 	container.style.transform = "rotate(" + deg + "deg)";
     
@@ -29,65 +24,44 @@ spinBtn.onclick = function () { //add reset botton
 
 function winAttraction(){
 
-    alert("Your prize is: ");
-    
     let position = deg%360;
 
     if(position < 22.5){
-        console.log("one wins");
-        link.setAttribute("href","#1");
+        link.setAttribute("href","https://letmegooglethat.com/?q=bars");
     }
 
     if(position > 22.5 && position < 67.5){
-        console.log("eight wins");
-        link.setAttribute("href","#8");
+        link.setAttribute("href","https://letmegooglethat.com/?q=new+video+games");
     }
 
     if(position > 67.5 && position < 112.5){
-        console.log("seven wins");
-        link.setAttribute("href","#7");
+        link.setAttribute("href","https://letmegooglethat.com/?q=Restaurant");
     }
 
     if(position > 112.5 && position < 157.5){
-        console.log("six wins");
-        link.setAttribute("href","#6");
+        link.setAttribute("href","https://letmegooglethat.com/?q=Coffee+shop");
     }
 
     if(position > 157.5 && position < 202.5){
-        console.log("five wins");
-        link.setAttribute("href","#5");
+        link.setAttribute("href","https://letmegooglethat.com/?q=Skydiving");
     }
 
     if(position > 202.5 && position < 247.5){
-        console.log("four wins");
-        link.setAttribute("href","#4");
+        link.setAttribute("href","https://letmegooglethat.com/?q=new+movies");
     }
 
     if(position > 247.5 && position < 292.5){
-        console.log("three wins");
-        link.setAttribute("href","#3");
+        link.setAttribute("href","https://letmegooglethat.com/?q=escape+rooms");
     }
 
     if(position > 292.5 && position < 337.5){
-        console.log("two wins");
-        link.setAttribute("href","#2");
+        link.setAttribute("href","https://www.netflix.com/il-en/");
     }
-
 
     link.setAttribute("class", "visible");
     resBtn.setAttribute("Class", "visible");
-
 }
 
-// resBtn.onclick = function () { //add reset botton
-     
-//     spinBtn.setAttribute("class","");
-//     link.setAttribute("class", "");
-//     resBtn.setAttribute("class","");
-
-//     link.setAttribute("href","#");
-
-// }
 
 
 
